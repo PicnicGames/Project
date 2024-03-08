f845d445faf2a83721eeb02f5d36204ecbad5fcd
nguyendoanhung <nguyendoanhungcv@gmail.com>
2024-03-08 20:58:27 +0700

    responsive

---
+++ pages/save_games/savedgame.js
@@ -0,0 +1,11 @@
+function showMenu() {
+    if(document.getElementById("header").style.overflow == 'hidden') {
+        document.getElementById("header").style.overflow = "visible";
+    } else {
+        document.getElementById("header").style.overflow = "hidden";
+    }
+};
+
+function searchButton() {
+    document.getElementById("search").style.width == '100vw';
+}
