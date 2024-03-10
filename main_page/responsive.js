ee1eb48c75ccc736ed051f5e692ffa3db9daaa38
nguyendoanhung <nguyendoanhungcv@gmail.com>
2024-03-10 23:25:20 +0700

    responsive game saved game

--- main_page/responsive.js
+++ main_page/responsive.js
@@ -15,3 +15,13 @@ function closeSearch() {
     document.getElementById("mb-search-bar").style.opacity = '0';
     document.getElementById("mb-search-bar").style.transform = 'translateX(100vw)';
 }
+
+function openCatagory() {
+    document.getElementById("catagory").style.display = 'block';
+    document.getElementById("game-panel").style.display = 'none';
+}
+
+function closeCatagory() {
+    document.getElementById("catagory").style.display = 'none';
+    document.getElementById("game-panel").style.display = 'block';
+}
