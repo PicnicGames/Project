3509f854d221208b55faf49ca4d0781836d96b8e
ZjjnN02 <dgbach2702@gmail.com>
2024-03-12 21:34:11 +0700

    user

---
+++ user2/user.js
@@ -0,0 +1,11 @@
+function signUp() {
+    document.getElementById('modal-container').classList.add('right-panel-active');
+    document.getElementById('overlay-image').src = 'https://i.pinimg.com/564x/c0/99/ac/c099ac0bb14e3fd693285cd28938ca76.jpg';
+    document.getElementById('close-button').style.color = "#000"
+};
+
+function signIn() {
+    document.getElementById('modal-container').classList.remove('right-panel-active');
+    document.getElementById('overlay-image').src = 'https://i.pinimg.com/564x/c1/6e/3c/c16e3c093406cf65f93fe527244cec63.jpg';
+    document.getElementById('close-button').style.color = "#fff"
+};
