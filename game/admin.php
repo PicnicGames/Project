11cdeb053dbe7ff961a32ac3ea3a459d55479fee
Haianh4705 <haianhnguyen472005@gmail.com>
2024-03-25 18:02:28 +0700

    all php

---
+++ game/admin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--=============== REMIXICONS ===============-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
+
+        <!--=============== SWIPER CSS ===============-->
+        <link rel="stylesheet" href="css/swiper-bundle.min.css">
+
+        <!--=============== GAME CSS ===============-->
+        <link rel="stylesheet" href="css/game.css">
+
+        <!--=============== BOOTSTRAP ===============-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <title>ADMIN</title>
+    </head>
+    <body>
+        <div class="container text-dark mt-3">
+            <h1>ADMIN PAGE</h1>
+        </div>
+        <div class="container">
+            <a href="php/view_game.php" class="btn btn-primary m-3">VIEW GAMES</a>
+            <a href="php/view_user.php" class="btn btn-primary m-3">VIEW USERS</a>
+        </div>
+        <div class="container">
+            <a href="php/add_game.php" class="btn btn-success m-3">ADD NEW GAME</a>
+        </div>
+    </body>
+</html>
---
+++ game/php/view_user.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--=============== REMIXICONS ===============-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.min.css">
+
+        <!--=============== SWIPER CSS ===============-->
+        <link rel="stylesheet" href="css/swiper-bundle.min.css">
+
+        <!--=============== GAME CSS ===============-->
+        <link rel="stylesheet" href="css/game.css">
+
+        <!--=============== BOOTSTRAP ===============-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <title>USER</title>
+    </head>
+    <body>
+        <div class="container mt-3">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>USERNAME</th>
+                        <th>EMAIL</th>
+                        <th>TYPE</th>
+                        <th>PASSWORD</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    require_once "utils.php";
+                    $sql = "select * from users";
+                    $res = res_sql_query($sql);
+                    for ($i = 0; $i < count($res); $i++) {
+                        $row = $res[$i];
+                        echo "<tr>
+                            <th>".$row['id']."</th>
+                            <th>".$row['username']."</th>
+                            <th>".$row['email']."</th>
+                            <th>".$row['type']."</th>
+                            <th>".$row['pwd']."</th>
+                        </tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
