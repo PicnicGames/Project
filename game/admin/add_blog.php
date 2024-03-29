41d5f84f3cc6e6031fb01cb24c7e6e9f876e3d64
nguyendoanhung <144453577+NguyenDoanHung1011@users.noreply.github.com>
2024-03-30 00:54:55 +0700

    Update add_blog.php

--- game/admin/add_blog.php
+++ game/admin/add_blog.php
@@ -40,7 +40,6 @@ if ($title != "") {
         <div class="header__content">
             <a href="index.html" class="header__logo">Picnic Play</a>
 
-            <!-- NONE -->
             <div class="header__user">
                 <div class="header__menu" id="header-menu">
                     <i class="ri-menu-fill"></i>
@@ -52,11 +51,11 @@ if ($title != "") {
     <!--==================== NAV ====================-->
     <nav class="navigation" id="navigation">
         <div class="nav__menu">
-            <a href="/project/subgame/admin.php" class="nav__logo">ADMIN PAGE</a>
+            <a href="admin.php" class="nav__logo">ADMIN PAGE</a>
 
             <ul class="nav__list">
                 <li class="nav__item">
-                    <a class="nav__link" href="/project/subgame/admin.php">
+                    <a class="nav__link" href="admin.php">
                         <i class="ri-home-5-line"></i> <span>Home</span>
                     </a>
                 </li>
@@ -99,7 +98,7 @@ if ($title != "") {
             </ul>
 
             <!-- USER -->
-            <div class="nav__user pt-3" id="registered">
+            <div class="nav__user pt-3">
                 <div class="nav__link">
                     <div class="user__container">
                         <i class="ri-user-fill"></i>
